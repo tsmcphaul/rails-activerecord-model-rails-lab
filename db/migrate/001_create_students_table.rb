86e3de5fb760f7002c9f9ff46447cb0685f62b64
Tammarra McPhaul <shaemc22@gmail.com>
2021-05-16 17:03:49 -0400

    commit tree 392d65ffbc7d6ab22102cbf7c42ce35a922fbf6a

---
+++ db/migrate/001_create_students_table.rb
@@ -0,0 +1,8 @@
+class CreateStudentsTable < ActiveRecord::Migration
+    def change
+      create_table :students do |t|
+        t.text :first_name
+        t.text :last_name
+      end
+    end
+  end 
